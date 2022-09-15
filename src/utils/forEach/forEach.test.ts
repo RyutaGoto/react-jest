19748b7d8c13d027bf0654f86124a72999f46fb1
RyutaGoto <kasa8gasa2rs6@gmail.com>
2022-09-15 23:48:37 +0900

    add comment for test

--- src/utils/forEach/forEach.test.ts
+++ src/utils/forEach/forEach.test.ts
@@ -9,18 +9,22 @@ describe('forEach', () => {
   });
 
   test('call length', () => {
+    // mockCallbackはちょうど2回だけ呼ばれたことをチェックする
     expect(mockCallback.mock.calls.length).toBe(2);
   });
 
   test('first argument of the first call', () => {
+    // mockCallback関数の1回目の呼び出しの1番目の引数は'0'であることをチェックする
     expect(mockCallback.mock.calls[0][0]).toBe(0);
   });
 
   test('first argument of the second call', () => {
+    // mockCallback関数の2回目の呼び出しの1番目の引数は'1'であることをチェックする
     expect(mockCallback.mock.calls[1][0]).toBe(1);
   });
 
   test('return value of the first call', () => {
+    // mockCallback関数の1回目の呼び出しの返り値は'42'であることをチェックする
     expect(mockCallback.mock.results[0].value).toBe(42);
   });
 });
