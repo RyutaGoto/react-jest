fb432b70825495dc8813d4d4d156b918d07422da
RyutaGoto <kasa8gasa2rs6@gmail.com>
2022-09-16 00:05:32 +0900

    fix test

--- src/utils/forEach/forEach.test.ts
+++ src/utils/forEach/forEach.test.ts
@@ -4,7 +4,7 @@ describe('forEach', () => {
   let mockCallback: jest.Mock<any, any>;
 
   beforeEach(() => {
-    mockCallback = jest.fn().mockReturnValue(200);
+    mockCallback = jest.fn().mockReturnValueOnce(200).mockReturnValueOnce(300);
     forEach([0, 1], mockCallback);
   });
 
@@ -15,16 +15,21 @@ describe('forEach', () => {
 
   test('first argument of the first call', () => {
     // mockCallback関数の1回目の呼び出しの1番目の引数は'0'であることをチェックする
-    expect(mockCallback.mock.calls[0][0]).toBe(200);
+    expect(mockCallback.mock.calls[0][0]).toBe(0);
   });
 
   test('first argument of the second call', () => {
     // mockCallback関数の2回目の呼び出しの1番目の引数は'1'であることをチェックする
-    expect(mockCallback.mock.calls[1][0]).toBe(200);
+    expect(mockCallback.mock.calls[1][0]).toBe(1);
   });
 
   test('return value of the first call', () => {
-    // mockCallback関数の1回目の呼び出しの返り値は'42'であることをチェックする
-    expect(mockCallback.mock.results[0].value).toBe(42);
+    // mockCallback関数の1回目の呼び出しの返り値は'200'であることをチェックする
+    expect(mockCallback.mock.results[0].value).toBe(200);
+  });
+
+  test('return value of the second call', () => {
+    // mockCallback関数の1回目の呼び出しの返り値は'300'であることをチェックする
+    expect(mockCallback.mock.results[1].value).toBe(300);
   });
 });
