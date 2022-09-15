dc17508d3847496a76ac785047a9d806b3410910
RyutaGoto <kasa8gasa2rs6@gmail.com>
2022-09-16 00:03:32 +0900

    fix method

--- src/utils/forEach/forEach.test.ts
+++ src/utils/forEach/forEach.test.ts
@@ -4,7 +4,7 @@ describe('forEach', () => {
   let mockCallback: jest.Mock<any, any>;
 
   beforeEach(() => {
-    mockCallback = jest.fn((x) => 42 + x);
+    mockCallback = jest.fn().mockReturnValue(200);
     forEach([0, 1], mockCallback);
   });
 
@@ -15,12 +15,12 @@ describe('forEach', () => {
 
   test('first argument of the first call', () => {
     // mockCallback関数の1回目の呼び出しの1番目の引数は'0'であることをチェックする
-    expect(mockCallback.mock.calls[0][0]).toBe(0);
+    expect(mockCallback.mock.calls[0][0]).toBe(200);
   });
 
   test('first argument of the second call', () => {
     // mockCallback関数の2回目の呼び出しの1番目の引数は'1'であることをチェックする
-    expect(mockCallback.mock.calls[1][0]).toBe(1);
+    expect(mockCallback.mock.calls[1][0]).toBe(200);
   });
 
   test('return value of the first call', () => {
