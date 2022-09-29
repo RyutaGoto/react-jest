0ca2565bb3345e782f323ca4a3311faea54b2ba4
RyutaGoto <kasa8gasa2rs6@gmail.com>
2022-09-29 23:38:41 +0900

    replace to partial mock function

--- src/utils/foo-bar-baz/foo-bar-baz.test.ts
+++ src/utils/foo-bar-baz/foo-bar-baz.test.ts
@@ -5,13 +5,16 @@ jest.mock('./foo-bar-baz', () => {
   return {
     __esModule: true,
     ...originalModule,
-    default: jest.fn(() => 'mocked baz'),
+    default: jest.fn(),
     foo: 'mocked foo',
   };
 });
 
+const mockFooBarBaz = defaultExport as jest.MockedFunction<typeof defaultExport>;
+
 test('should do a partial mock', () => {
-  const defaultExportResult = defaultExport();
+  mockFooBarBaz.mockImplementation(() => 'mocked baz');
+  const defaultExportResult = mockFooBarBaz();
   expect(defaultExportResult).toEqual('mocked baz');
   expect(foo).toBe('mocked foo');
   expect(bar()).toBe('bar');
